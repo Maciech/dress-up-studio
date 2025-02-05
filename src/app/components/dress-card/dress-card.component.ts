5a33ada7ed83562a7279a00cd78a489c61f9dcf7
Maciech <maciek.chodacki@wp.pl>
2025-02-05 20:15:40 +0100

    master change

--- src/app/components/dress-card/dress-card.component.ts
+++ src/app/components/dress-card/dress-card.component.ts
@@ -59,20 +59,6 @@ export class DressCardComponent {
     return [...new Set(availability.map((a) => a.size))];
   }
 
-  getColorHex(color: COLOR): string {
-    const colorMap: { [key in COLOR]: string } = {
-      [COLOR.RED]: 'red', // Bootstrap danger color
-      [COLOR.BLUE]: 'blue', // Bootstrap primary color
-      [COLOR.GREEN]: 'green', // Bootstrap success color
-      [COLOR.BLACK]: 'black', // Bootstrap dark color
-      [COLOR.WHITE]: 'white', // Bootstrap secondary (grayish for contrast)
-      [COLOR.PINK]: 'pink',
-    };
-    console.log(colorMap[color]);
-    return colorMap[color] || '#6c757d'; // Default gray if not found
-  }
-
-
   getLowestPrice(dressAvailability: DressAvailability[]) {
     if (!dressAvailability || dressAvailability.length === 0) return 'N/A';
     const prices = dressAvailability.map((a) => a.price);
